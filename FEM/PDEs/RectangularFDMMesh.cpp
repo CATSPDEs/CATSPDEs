5a34915001eec7a3bb46c01d9994c11f5eae99b1
andrew simonov <ozimantius@gmail.com>
2016-03-08 19:24:25 +0600

    add hx, hy members

--- FEM/PDEs/RectangularFDMMesh.cpp
+++ FEM/PDEs/RectangularFDMMesh.cpp
@@ -9,14 +9,14 @@ RectangularFDMMesh::RectangularFDMMesh(Point startPoint, Point endPoint, unsigne
 {
 	_width = abs(endPoint.x - startPoint.x);
 	_length = abs(endPoint.y - startPoint.y);
-	double hY = _length / nY;
-	double hX = _width / nX;
+	_hY = _length / nY;
+	_hX = _width / nX;
 	_nodes.reserve(nX*nY);
 	for (int i = 0; i < nX; i++)
 	{
 		for (int j; j < nY; j++)
 		{
-			AbstractNode node(startPoint.x + i*hX, startPoint.y + j*hY, 0);
+			AbstractNode node(startPoint.x + i*_hX, startPoint.y + j*_hY, 0);
 			_nodes.push_back(node);
 		}
 	}
--- FEM/PDEs/RectangularFDMMesh.hpp
+++ FEM/PDEs/RectangularFDMMesh.hpp
@@ -6,6 +6,8 @@ class RectangularFDMMesh : public AbstractMesh
 	double _length;
 	unsigned _horDotNumber;
 	unsigned _vertDotNumber;
+	double _hX;
+	double _hY;
 public:
 	unsigned nodeMapping(unsigned) const;
 	RectangularFDMMesh(Point, Point, unsigned, unsigned);
