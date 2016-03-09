bba51e01928eb748741a8f9289b0389b27cadd78
Alexander <alexander@56th.ru>
2016-03-09 21:38:36 +0600

    Implement Function of space-time

--- FEM/Tools/Function.hpp
+++ FEM/Tools/Function.hpp
@@ -1,16 +1,28 @@
 #pragma once
 #include <iostream>
+#include <stdexcept>
 #include "Point.hpp"
 
-typedef double(*Function)(Point const &); // function pointers
+typedef double(*Function)(Point const &, double); // function pointer
+
+inline double zeroFunc(Point const &, double) { return 0.; }
+inline double oneFunc(Point const &, double) { return 1.; }
 
 // What does it mean to read Function from stdin? 
-// No sense in reading pointers, so we define our function as constant zero 
+// No sense in saving pointers
 
-inline double zeroFunc(Point const &) { return 0.; }
-inline double oneFunc(Point const &) { return 1.; }
-
-inline std::istream& operator>>(std::istream& input, Function f) {
-	f = zeroFunc;
+inline std::istream& operator>>(std::istream& input, Function& f) {	
+	unsigned n;
+	input >> n;
+	if (n == 0U) f = zeroFunc;
+	else if (n == 1U) f = oneFunc;
+	else throw std::logic_error("you want to read something wierd");
 	return input;
+}
+
+inline std::ostream& operator<<(std::ostream& output, Function const & f) {
+	if (f == zeroFunc) output << 0;
+	else if (f == oneFunc) output << 1;
+	else throw std::logic_error("you want to save something wierd");
+	return output;
 }
